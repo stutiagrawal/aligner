aeb81c545a410e59955df3e8c097142e542b1773
Ubuntu <ubuntu@align-mosaik-32-core.novalocal>
2014-10-23 21:14:44 +0000

    generate graphs

--- collect_metrics.py
+++ collect_metrics.py
@@ -3,9 +3,18 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import multiprocessing
+import argparse
+
+def create_graph(x_axis_data, y_axis_data, line_label, fig_name, x_axis_label = "", y_axis_label = ""):
+    line, = plt.plot(x_axis_data, y_axis_data, 'rs')
+    plt.legend([line],[line_label])
+    plt.xlabel(x_axis_label)
+    plt.ylabel(y_axis_label)
+    plt.savefig(fig_name)
+    plt.clf()
 
 def collect_metrics(dirname, program):
-    max_proc = max(32, multiprocessing.cpu_cout())
+    max_proc = max(32, multiprocessing.cpu_count())
 
     threads = [0] * max_proc
     time_elapsed = [0] * max_proc
@@ -24,15 +33,21 @@ def collect_metrics(dirname, program):
                         metric = line
 
                 metric = metric.split()
-                if(len(metric) >= 9):
+                #`print metric
+		if(len(metric) >= 9):
                     user = float(metric[3].replace("user", ""))/3600.0
 
                     system = float(metric[4].replace("system", ""))/3600.0
 
                     elapsed = metric[5].replace("elapsed", "")
                     if not (elapsed == "0:00.00"):
-                        (h, m, s) = metric[5].replace("elapsed", "").split(":")
-                        elapsed = (float(h)*3600 + float(m)*60 + float(s))/3600.0
+			if("." not in elapsed):
+			    h, m , s = elapsed.split(":")
+                        else:
+			    elapsed = elapsed.replace(".", ":")
+			    h = 0
+			    m,s,ms = elapsed.split(":")
+			elapsed = (float(h)*3600 + float(m)*60 + float(s))/3600
                     else:
                         elapsed = 0.00
 
@@ -43,21 +58,24 @@ def collect_metrics(dirname, program):
                     mem = metric[8].replace("maxresident)k", "")
                     num_threads = filename.split("_")[-1]
 
-                pos = int(num_threads) - 1
+		pos = int(num_threads) - 1
                 threads[pos] = int(num_threads)
                 time_elapsed[pos] = elapsed
                 cpu_time[pos] = float(cpu)
                 user_time[pos] = float(user)
                 mem_time[pos] = float(mem)
 
-    #elapsed_line, = plt.plot(threads, time_elapsed, 'bs')
-    cpu_line, = plt.plot(threads, cpu_time, 'rs')
-    #user_line, = plt.plot(threads, user_time, 'g--')
-    #plt.legend([elapsed_line, cpu_line, user_line], ['elapsed', 'cpu', 'user'])
-    plt.legend([cpu_line], ['cpu'])
-    plt.savefig("%s_timing.png" %(program))
-    plt.clf()
+    create_graph(threads, time_elapsed, "Elapsed time", "%s_elapsed" %(program), "No. of threads", "Time(h)")
+    create_graph(threads, cpu_time, "CPU time", "%s_CPU_time" %(program), "No. of threads", "Time(h)")
+    create_graph(threads, user_time, "User_time", "%s_user_time" %(program), "No. of threads", "Time(h)")
+    create_graph(threads, mem_time, "MEM_used", "%s_mem_used" %(program), "No. of threads", "Memory(K)")
 
-    #print "%s\t%s\t%s\t%s\t%s\t%s" %(num_threads, user, system, elapsed, cpu, mem)
+if __name__ == "__main__":
 
-collect_metrics(".", "novo")
+    parser = argparse.ArgumentParser(prog='collect_metrics.py', description='Plot graphs')
+    parser.add_argument('dirname', default=".", help='path to directory with log files')
+    parser.add_argument('program', help='Tool used to generate results')
+    args = parser.parse_args()
+
+    collect_metrics(args.dirname, args.program)
+
